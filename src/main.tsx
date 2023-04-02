18e14f6700542fa368b1476d782f071d3cd75738
NiiXu <chenhengxv@outlook.com>
2023-04-02 15:32:00 +0800

    创建404

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {createBrowserRouter,RouterProvider, Router} from 'react-router-dom'
+import {ErrorPage} from './components/ErrorPage';
 // import {App} from './App'
 
 const router = createBrowserRouter([
     {
         path:'/',
-        element:<div>root</div>
+        element:<div>root</div>,
+        errorElement:<ErrorPage></ErrorPage>
     }
 ])
 
