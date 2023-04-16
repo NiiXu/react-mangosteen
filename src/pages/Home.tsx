dce6151c9396a97ebab950a986c51c1604d80263
NiiXu <chenhengxv@outlook.com>
2023-04-16 13:53:45 +0800

    #repair

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,3 +3,5 @@ import React from 'react';
 export const Home:React.FC = ()=>{
     return <div className={'text-6xl'}>Home</div>
 }
+
+
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -6,7 +6,7 @@ import {Welcome1} from '../pages/Welcome1';
 import {Welcome2} from '../pages/Welcome2';
 import {Welcome3} from '../pages/Welcome3';
 import {Welcome4} from '../pages/Welcome4';
-import {Home} from '../pages/home';
+import {Home} from '../pages/Home';
 import {WelcomeLayout} from '../layout/WelcomeLayout';
 
 
