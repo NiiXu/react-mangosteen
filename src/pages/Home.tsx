b802d25688b53d2baf2117faf7a9856b3b5e5d3a
NiiXu <chenhengxv@outlook.com>
2023-04-16 14:35:57 +0800

    完成首页样式

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,25 +1,21 @@
 import React from 'react';
-import useSWR, {mutate} from 'swr';
-import axios from 'axios';
-
-const fetcher = (path:string) => {
-  return axios.get<{message:string}>(path)
-}
+import p from '../assets/images/pig.svg'
+import add from '../assets/icons/add.svg'
 export const Home:React.FC = ()=>{
-    const {data,error,isValidating,mutate} = useSWR('http://121.196.236.94:3000',fetcher)
-    const isLoading = !data && ! error
-    const onClick = async ()=>{
-        const data = await axios.post('xxx')
-        mutate(data)
-    }
 
-    if (error){
-        return <div>error</div>
-    }
-    if (!data){
-        return <div>loding..</div>
-    }
-    return <div>{data.data.message}</div>
+    return <div>
+        <div className="flex justify-center items-center">
+            <img className="mt-20vh mb-20vh" width="128" height="130" src={p} />
+        </div>
+        <div className="px-16px">
+            <button className="h-48px w-100% bg-#5C33BE b-none text-white
+            rounded-8px">开始记账</button>
+        </div>
+        <button className="w-56px h-56px bg-#5C33BE rounded-50% b-none text-white
+        text-6xl fixed bottom-16px right-16px">
+            <img src={add} className="max-w-100% max-h-100%" />
+        </button>
+    </div>
 }
 
 
--- src/pages/Welcome1.tsx
+++ src/pages/Welcome1.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import p1 from '../assets/images/welcome1.svg'
+import p1 from '../assets/images/pig.svg'
 export const Welcome1: React.FC = () => {
     return (
         <div  className="text-center">
