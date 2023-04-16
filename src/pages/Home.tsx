b2d0a22063fe81b4da488d580c108f8361b11199
NiiXu <chenhengxv@outlook.com>
2023-04-16 14:15:20 +0800

    安装 swr 和 axios

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,7 +1,25 @@
 import React from 'react';
+import useSWR, {mutate} from 'swr';
+import axios from 'axios';
 
+const fetcher = (path:string) => {
+  return axios.get<{message:string}>(path)
+}
 export const Home:React.FC = ()=>{
-    return <div className={'text-6xl'}>Home</div>
+    const {data,error,isValidating,mutate} = useSWR('http://121.196.236.94:3000',fetcher)
+    const isLoading = !data && ! error
+    const onClick = async ()=>{
+        const data = await axios.post('xxx')
+        mutate(data)
+    }
+
+    if (error){
+        return <div>error</div>
+    }
+    if (!data){
+        return <div>loding..</div>
+    }
+    return <div>{data.data.message}</div>
 }
 
 
